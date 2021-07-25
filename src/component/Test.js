0930094eed7692beff38167cc9b343aee62b39a3
Soner Koçak <sonerkocak@gmail.com>
2021-07-26 00:23:21 +0300

    added onClick mutate to Test.js

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,15 +1,12 @@
 export default function Test({ baslik, children }) {
 
     function handleButtonClick() {
-        //baslik = 'a';
-        return () => {
-            console.log('tıklandı.');
-        }
+        baslik = 'a';
     }
 
     return (
       <div>
-          <button onClick={handleButtonClick()}>Tıkla</button>
+          <button onClick={() => handleButtonClick()}>Tıkla</button>
           <h5>{baslik}</h5>
           {children}
       </div>
