4a0a0fa851c0a9a4d2401319ff78051b5e578894
Soner Koçak <sonerkocak@gmail.com>
2021-07-26 00:13:36 +0300

    added onClick run fn to Test.js

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,7 +1,15 @@
 export default function Test({ baslik, children }) {
 
+    function handleButtonClick() {
+        //baslik = 'a';
+        return () => {
+            console.log('tıklandı.');
+        }
+    }
+
     return (
       <div>
+          <button onClick={handleButtonClick()}>Tıkla</button>
           <h5>{baslik}</h5>
           {children}
       </div>
