ce5f25e904c7a20ffa2f49b88ab43a76adbc8564
Soner Koçak <sonerkocak@gmail.com>
2021-07-26 01:26:11 +0300

    added text and liste to Test.js

--- src/component/Test.js
+++ src/component/Test.js
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 export default function Test({ baslik, children }) {
     const [sayac, setSayac] = useState(1);
     const [sayacKare, setSayacKare] = useState(1);
+    const [text, setText] = useState('');
+
+    const [liste, setListe] = useState(['a', 'b']);
 
     useEffect(() => {
         console.log('useEffect Sayaç = ', sayac);
@@ -24,8 +27,23 @@ export default function Test({ baslik, children }) {
     return (
       <div>
           <button onClick={() => handleButtonClick()}>Tıkla {sayac}</button>
+          <br />
+          <input value={text} onChange={e => setText(e.target.value)} />
+          <button onClick={() => {
+              const newListe = [...liste];
+              // const newListe = liste.filter(val => true);
+              newListe.unshift(text);
+              setListe(newListe);
+              setText('');
+          }}>Tıkla {sayac}</button>
           <h5>{baslik} {sayacKare}</h5>
           {children}
+          <br />
+          <ul>
+              {liste.map(val => (
+                  <li>{val}</li>
+              ))}
+          </ul>
       </div>
     );
 }
